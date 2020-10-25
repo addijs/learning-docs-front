347673f6d04f75c46b058287568de8c60aa32f33
Adilson Junior <aj.adilsonjunior1@gmail.com>
2020-10-24 23:22:59 -0300

    Adiciona service de usuario

---
+++ src/app/views/user-auth/user.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from './user';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  private APP_URL: string = 'http://localhost:3333/users';
+
+  constructor(private http: HttpClient) {}
+
+  signUp(user: User): Observable<User> {
+    return this.http.post<User>(this.APP_URL, user);
+  }
+
+  login(email: string): Observable<User[]> {
+    return this.http.get<User[]>(this.APP_URL + `?email=${email}`);
+  }
+}
